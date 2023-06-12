6c03183ec14ae76f80a17805fd620501509a8aac
dreamjbj1 <999888@naver.com>
2023-06-12 17:18:03 +0900

    20230607

---
+++ 20230607.java/src/object/Book.java
@@ -0,0 +1,12 @@
+package object;
+
+class Book {
+	int bookNumber;
+	String bookTitle;
+
+	Book(int bookNumber, String bookTitle) {
+		this.bookNumber = bookNumber;
+		this.bookTitle = bookTitle;
+	}
+}
+
